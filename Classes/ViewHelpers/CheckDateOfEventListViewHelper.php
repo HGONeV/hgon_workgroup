3e74444090ece27eeb747606a92b051227c0f4bb
Maximilian Faessler <maximilian@faesslerweb.de>
2019-12-04 15:30:01 +0000

    [EDIT] Date control of events

---
+++ Classes/ViewHelpers/CheckDateOfEventListViewHelper.php
@@ -0,0 +1,51 @@
+<?php
+namespace HGON\HgonWorkgroup\ViewHelpers;
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
+/**
+ * CheckDateOfEventListViewHelper
+ *
+ * @author Maximilian Fäßler <maximilian@faesslerweb.de>
+ * @copyright HGON
+ * @package HGON_HgonWorkgroup
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class CheckDateOfEventListViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+    /**
+     * Checks if minimum one event in list is not expired
+     *
+     * @param array | \TYPO3\CMS\Extbase\Persistence\ObjectStorage $eventList
+     *
+     * @return boolean
+     */
+    public function render($eventList)
+    {
+        // if minimum one element is not expired, return true
+        foreach ($eventList as $event) {
+
+            $date = $event->getEnd() ? $event->getEnd() : $event->getStart();
+
+            /** @var \RKW\RkwEvents\Domain\Model\Event $event */
+            if ($date >= time()) {
+                return true;
+                //===
+            }
+        }
+
+        return false;
+        //===
+    }
+}
